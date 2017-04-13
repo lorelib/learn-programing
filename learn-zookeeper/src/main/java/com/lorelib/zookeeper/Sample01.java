fe50e2ab3db833fb6a343a11f9a2ea5f9fdc2d8c
listening <893979653@qq.com>
2017-04-13 18:51:32 +0800

    zookeeper helloworld 无密码密钥

---
+++ learn-zookeeper/src/main/java/com/lorelib/zookeeper/Sample01.java
@@ -0,0 +1,37 @@
+package com.lorelib.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author listening
+ * @description Sample01:
+ * @create 2017 04 13 18:21.
+ */
+public class Sample01 implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException {
+        ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 5000, new Sample01());
+        System.out.println(zk.getState());
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ZK session established.");
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        System.out.println("Receive watched event: " + watchedEvent);
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            connectedSemaphore.countDown();
+            System.out.println("Connected to ZK");
+        }
+    }
+}
