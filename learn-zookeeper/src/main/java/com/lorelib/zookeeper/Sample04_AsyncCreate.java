a24f7785b513b81a0a76d823228ba328983883e5
listening <893979653@qq.com>
2017-04-19 23:02:19 +0800

    异步创建znode

---
+++ learn-zookeeper/src/main/java/com/lorelib/zookeeper/Sample04_AsyncCreate.java
@@ -0,0 +1,16 @@
+package com.lorelib.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+/**
+ * @author listening
+ * @description Sample04_AsyncCreate:
+ * @create 2017 04 19 13:37.
+ */
+public class Sample04_AsyncCreate implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+}
