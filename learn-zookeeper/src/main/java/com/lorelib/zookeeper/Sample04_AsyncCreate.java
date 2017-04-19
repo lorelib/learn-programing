0945cd846fb2f12b24859053359b6716a909a5ec
listening <893979653@qq.com>
2017-04-20 00:00:52 +0800

    异步创建znode

--- learn-zookeeper/src/main/java/com/lorelib/zookeeper/Sample04_AsyncCreate.java
+++ learn-zookeeper/src/main/java/com/lorelib/zookeeper/Sample04_AsyncCreate.java
@@ -1,7 +1,9 @@
 package com.lorelib.zookeeper;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author listening
@@ -9,8 +11,39 @@ import org.apache.zookeeper.Watcher;
  * @create 2017 04 19 13:37.
  */
 public class Sample04_AsyncCreate implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2181",
+                5000, new Sample04_AsyncCreate());
+        connectedSemaphore.await();
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                new IStringCallback(), "I am context.");
+
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL,
+                new IStringCallback(), "I am context.");
+
+        zooKeeper.create("/zk-test-ephemeral-", "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
+                new IStringCallback(), "I am context.");
+
+        Thread.sleep(10000);
+    }
+
     @Override
     public void process(WatchedEvent watchedEvent) {
-
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            connectedSemaphore.countDown();
+        }
+    }
+}
+class IStringCallback implements AsyncCallback.StringCallback {
+    @Override
+    public void processResult(int rc, String path, Object ctx, String name) {
+        System.out.println("Create path result: [" + rc + ", " + path + ", "
+                + ctx + ", real path name: " + name);
     }
 }
