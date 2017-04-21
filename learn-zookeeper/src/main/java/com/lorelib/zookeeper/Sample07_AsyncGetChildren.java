006bb99a97c0b68bfaf29fcc3633cbc57c88e324
listening <893979653@qq.com>
2017-04-22 00:41:12 +0800

    同步获取子节点列表

---
+++ learn-zookeeper/src/main/java/com/lorelib/zookeeper/Sample07_AsyncGetChildren.java
@@ -0,0 +1,55 @@
+package com.lorelib.zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by listening on 2017/4/22.
+ */
+public class Sample07_AsyncGetChildren implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        zooKeeper = new ZooKeeper("127.0.0.1:2181", 5000,
+                new Sample07_AsyncGetChildren());
+        connectedSemaphore.await();
+        String path = "/zk-book";
+        zooKeeper.create(path, "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+
+        zooKeeper.create(path + "/c1", "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL);
+
+        List<String> childrenList = zooKeeper.getChildren(path, true);
+        System.out.println("childrenList: " + childrenList);
+
+        zooKeeper.create(path + "/c2", "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL);
+
+        Thread.sleep(10000);
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath()) {
+                connectedSemaphore.countDown();
+            } else if (watchedEvent.getType() == Event.EventType.NodeChildrenChanged) {
+                try {
+                    System.out.println("ReGet Child: " + zooKeeper.getChildren(watchedEvent.getPath(), true));
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
