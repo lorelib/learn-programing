28f7607f03260af60b36cc9147e2fc172722e2ab
listening <893979653@qq.com>
2017-07-21 16:44:16 +0800

    activemq点对点P2P模式实践

---
+++ learn-mq/src/main/scala/com/lorelib/mq/activemq/p2p/ActiveMQConfig.scala
@@ -0,0 +1,15 @@
+package com.lorelib.mq.activemq.p2p
+
+import org.apache.activemq.ActiveMQConnection
+
+/**
+  * @description ActiveMQConfig: 
+  * @author listening
+  * @create 2017 07 21 下午3:52.
+  */
+
+trait ActiveMQConfig {
+  val USERNAME: String = ActiveMQConnection.DEFAULT_USER
+  val PASSWORD: String = ActiveMQConnection.DEFAULT_PASSWORD
+  val BROKEURL: String = ActiveMQConnection.DEFAULT_BROKER_URL
+}
